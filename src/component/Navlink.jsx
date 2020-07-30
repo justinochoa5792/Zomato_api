b9a0e54023ae1d952a1ac534ef9bb4ed8c42d1d1
Justin Ochoa <just.och92@gmail.com>
2020-07-29 20:16:24 -0400

    more styling

--- src/component/Navlink.jsx
+++ src/component/Navlink.jsx
@@ -4,6 +4,11 @@ class Navlink extends Component {
   render() {
     return (
       <div className="nav">
+        <h4
+          style={{ color: " white", fontSize: "x-large", fontWeight: "bold" }}
+        >
+          ZTRR
+        </h4>
         <Link to="/">Back</Link>
       </div>
     );
